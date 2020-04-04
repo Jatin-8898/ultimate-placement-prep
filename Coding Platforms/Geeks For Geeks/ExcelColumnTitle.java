a81734f103a016d710e0d7d8c1e77200c12e5ea6
Jatin Varlyani <varlyanijatin88@gmail.com>
2020-04-04 14:12:20 +0530

    Added GFG

--- Coding Platforms/Geeks For Geeks/ExcelColumnTitle.java
+++ Coding Platforms/Geeks For Geeks/ExcelColumnTitle.java
@@ -1,6 +1,20 @@
 class ExcelColumnTitle{
 	public static void main(String[] args) {
-		
-		
-	}
+        String s = "AA";
+        System.out.println( titleToNumber(s));
+    }
+    
+    public static int titleToNumber(String s) {
+                
+        int result  = 0;
+        
+        for (int i = 0; i < s.length(); i++){   //for s = A => 65-65=0 => Add 1 to it
+    
+            result *= 26;                       //For AA = 1*26 = 26 + 1 = 27
+            result += ((s.charAt(i) - 'A') + 1);    
+        }
+    
+        return result;
+    }
+
 }
